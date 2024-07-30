bdf631fcf8f7e5b9762ba79b99b1c7ffc23d238c
JohnnyUtah666 <porksloppp@gmail.com>
2024-07-29 23:27:42 -0700

    start writing function for Add Task button

--- src/tasksTab.js
+++ src/tasksTab.js
@@ -4,6 +4,15 @@ const allTasks = () => {
     tasksHeader.setAttribute('id', 'tasksHeader');
     tasksHeader.textContent = 'All Tasks'
     content.appendChild(tasksHeader);
+
+    let newTaskButton = document.createElement('button');
+    newTaskButton.setAttribute('id', 'newTaskButton');
+    newTaskButton.textContent = "+ New Task";
+    content.appendChild(newTaskButton);
+
+        const addTask = () => {
+            
+        }
 }
 
 export {allTasks}
