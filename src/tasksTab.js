a4b53435e73f16eacc26867e4ea40f43e1abfafe
JohnnyUtah666 <porksloppp@gmail.com>
2024-07-30 23:28:50 -0700

    write function that populates display

--- src/tasksTab.js
+++ src/tasksTab.js
@@ -13,6 +13,45 @@ const allTasks = () => {
         const addTask = () => {
             
         }
+    
+}
+
+const displayAllTasks = (array) => {
+        
+    
+    
+    for (let item of array) {
+    let taskCard = document.createElement('div');
+    taskCard.setAttribute('id', 'taskCard');
+    content.appendChild(taskCard);
+
+    let displayName = document.createElement('div');
+    let displayDueDate = document.createElement('div');
+    let displayCompleted = document.createElement('div');
+    let displayNotes = document.createElement('div');
+
+    displayName.setAttribute('id', 'displayName');
+    displayDueDate.setAttribute('id', 'displayDueDate');
+    displayCompleted.setAttribute('id', 'displayCompleted');
+    displayNotes.setAttribute('id', 'displayNotes');
+
+    let displayNameContent = document.createTextNode(item.name);
+    let displayDueDateContent = document.createTextNode(item.dueDate);
+    let displayCompletedContent = document.createTextNode(item.completed);
+    let displayNotesContent = document.createTextNode(item.notes);
+
+    displayName.appendChild(displayNameContent);
+    displayDueDate.appendChild(displayDueDateContent);
+    displayCompleted.appendChild(displayCompletedContent);
+    displayNotes.appendChild(displayNotesContent);
+
+    taskCard.appendChild(displayName);
+    taskCard.appendChild(displayDueDate);
+    taskCard.appendChild(displayCompleted);
+    taskCard.appendChild(displayNotes);
+
+}
 }
 
 export {allTasks}
+export {displayAllTasks}
