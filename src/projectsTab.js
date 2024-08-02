2582443c508a81f15719180b511fa91736a1a8c0
JohnnyUtah666 <porksloppp@gmail.com>
2024-08-02 13:25:44 -0700

    add modal that will allow user to create a new project

--- src/projectsTab.js
+++ src/projectsTab.js
@@ -1,9 +1,38 @@
+const createProjectModal = () => {
+    let projectModal = document.createElement('dialog');
+    document.body.appendChild(projectModal);
+    let projectModalText = document.createTextNode("Here's the modal");
+
+    let closeProjectModal = document.createElement('button');
+    closeProjectModal.textContent = 'X';
+    closeProjectModal.addEventListener('click', () => {
+        projectModal.remove();
+    });
+
+    projectModal.appendChild(projectModalText);
+    projectModal.appendChild(closeProjectModal);
+    projectModal.showModal()
+}
+
+const newProject = () => {
+    createProjectModal();
+}
+
 const allProjects = () => {
     document.getElementById("content").innerHTML = "";
     let projectsHeader = document.createElement('div');
     projectsHeader.setAttribute('id', 'projectsHeader');
     projectsHeader.textContent = 'Projects'
     content.appendChild(projectsHeader);
+
+    let newProjectButton = document.createElement('button');
+    newProjectButton.setAttribute('id', 'newProjectButton');
+    newProjectButton.textContent = "+ New Project";
+    content.appendChild(newProjectButton);
+    newProjectButton.addEventListener('click', () => {
+        newProject();
+    })
+
 }
 
 export {allProjects}
